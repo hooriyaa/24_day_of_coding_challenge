d76b742f8e8bf1c43163c046840446736ff30084
hooriya <parih3080@gmail.com>
2024-04-30 23:39:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+"use strict";
+// QUESTION 70:
+function numberInLoop() {
+    for (let a = 0; a <= 5; a++) {
+        console.log(a);
+    }
+    ;
+}
+;
+numberInLoop();
+// QUESTION 71:
+// reassign let varaible:
+let userName = "hooriya";
+userName = "Mahnoor";
+console.log(userName);
+// reassign const varaible:
+const z = "Malahim";
+try {
+    z = "google";
+    console.log(z);
+}
+catch (error) {
+    console.log("Error: const varaible can't be reassign!");
+}
+// QUESTION 72:
+{
+    let blocklet = "Visible inside the block";
+    let blockconst = "also only inside the block";
+    console.log(blocklet);
+    console.log(blockconst);
+}
+try {
+    console.log(blocklet);
+}
+catch (error) {
+    console.log("blocklet: Can't be assign outside the block!! ");
+}
+try {
+    console.log(blockconst);
+}
+catch (error) {
+    console.log("blockconst: Can't be assign outside the block!! ");
+}
